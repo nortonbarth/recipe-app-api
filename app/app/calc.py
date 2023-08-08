68b8a06e79fe642582efa45590ee360a2d8cc716
Norton <nortonbarth@gmail.com>
2023-08-08 11:24:47 -0300

    fail to create the database

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Sum 2 values
+"""
+
+
+def add(x, y):
+    "Add x and y and return the reuslt"
+
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return the result"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""Sample test"""
+
+from django.test import SimpleTestCase
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    """Test calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_number(self):
+        """Subtract 2 numbers"""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
