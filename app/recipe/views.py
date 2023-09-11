20e48c6fa0552f937f6480cc1fabc3fdfe329a6f
Norton <nortonbarth@gmail.com>
2023-09-11 15:17:38 -0300

    Tags implementation

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,11 +2,19 @@
 Views for the recipe APIs
 """
 
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,
+)
+
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
+
 from . import serializers
 
 
@@ -32,3 +40,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
         return super().perform_create(serializer)
+
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage Tags in the database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
